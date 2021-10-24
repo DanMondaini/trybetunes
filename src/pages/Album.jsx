ce0659a6cde42aefb2b24b75533e914488e87b3a
DanMondaini <danielmondaini@outlook.com>
2021-10-24 13:26:34 -0300

    Req 1

---
+++ src/pages/Album.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        <h2>Album Page</h2>
+      </div>
+    );
+  }
+}
+
+export default Album;
