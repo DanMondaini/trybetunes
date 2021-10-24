f4c6d8a660527fd92c51edb343b53380cf82418f
DanMondaini <danielmondaini@outlook.com>
2021-10-24 14:37:24 -0300

    Req 3

--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class ProfileEdit extends React.Component {
   render() {
     return (
       <div data-testid="page-profile-edit">
-        <h2>ProfileEdit Page</h2>
+        <Header />
       </div>
     );
   }
